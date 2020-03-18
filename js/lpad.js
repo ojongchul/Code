cc357a3293cfdd19687b8789b28e211e08d78ea2
jongchul <ojongchul@gmail.com>
2020-03-18 11:59:04 +0900

    M  js/lpad.js

--- js/lpad.js
+++ js/lpad.js
@@ -1,11 +1,14 @@
-function lpad(str, position = 'left', padstr = '0', length = 3)
+// PAD 함수
+// pad(3) => "003"
+// pad(3, "right") => "300"
+function pad(str, position = 'left', padstr = '0', length = 3)
 {
 	if(str == undefined)
 		return;
 	let pad = ('' + padstr).repeat(length);
 	if(position == 'left') {
 		str = pad + str;
-		return str.substring(str.length - length, length);
+		return str.substring(str.length - length, str.length);
 	} else {
 		str += pad;
 		return str.substring(0, length);
